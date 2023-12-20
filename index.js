4f4b74f8a03c7ad180090b91d49739b9d6cfe92b
mangeshzffytech <mangesh@ziffytech.com>
2023-12-20 18:06:58 +0530

    index file code

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 require('dotenv').config()
+const http =require('http');
 const express = require('express')
+const {MongoClient} = require('mongodb');
+const database='ziffytech';
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => {
-  http.createServer((req,resp)=>{
-    resp.writeHead(200,{'Content-Type':'application/json'});
-    resp.write(JSON.stringify({name:'mangesh mane',email:'mane@gmail.com'}));
-    resp.end();
-  }).listen(6000);
-})
+async function getData(){
+  let result = await client.connect();
+  let db=result.db(database);
+  let collection=db.collection('ziffy');
+ 
+  let response = await collection.find({}).toArray();
+  console.log(response);
+}
+getData();
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World sss!')
-// })
+
+http.createServer((req,resp)=>{
+  resp.writeHead(200,{'Content-Type':'application/json'});
+  resp.write(JSON.stringify({name:'mangesh mane kkkk',email:'mane@gmail.com'}));
+  resp.end();
+}).listen(5000);
+
+
 
 app.get('/youtype', (req, res)=>{
 res.send('Mangesh Mane')
